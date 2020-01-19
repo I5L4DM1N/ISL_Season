ecba803ccc3eff2926bd9f660c1745db5317ca75
I5L4DM1N <I5L4DM1N@gmail.com>
2020-01-19 03:17:05 +0100

    installed django-heroku updated settings file

--- ISL_Season/settings.py
+++ ISL_Season/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -153,3 +154,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 #MEDIA_URL = '/Users/anton/OneDrive/ISL_Season/ISL_Season/media/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+django_heroku.setting(locals())
