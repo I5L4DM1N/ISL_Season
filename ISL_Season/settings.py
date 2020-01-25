b08e0145d8494d28e113159e92c35f3891e08a1d
I5L4DM1N <I5L4DM1N@gmail.com>
2020-01-25 12:19:31 +0100

    update ssl workaround

--- ISL_Season/settings.py
+++ ISL_Season/settings.py
@@ -174,4 +174,4 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
 
-del DATABASES['default']['OPTIONS']['sslmode']
+del DATABASES['OPTIONS']['sslmode']
