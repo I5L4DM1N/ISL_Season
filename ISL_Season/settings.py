c53072b7e1f8fb2ab932b2d7fc63f64b3cfd089c
I5L4DM1N <I5L4DM1N@gmail.com>
2020-01-25 12:10:23 +0100

    disableing ssl heroku db

--- ISL_Season/settings.py
+++ ISL_Season/settings.py
@@ -173,3 +173,5 @@ AWS_S3_REGION_NAME = 'eu-north-1'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
+
+del DATABASES['default']['OPTIONS']['sslmode']
